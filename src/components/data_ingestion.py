cd69c72f2cdaebf3de2b9bd2b3c5da50c94c22e2
VishakhaSingh123 <vishakhasinghmdw@gmail.com>
2026-01-31 00:55:03 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -13,6 +13,10 @@ from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
+
 
 @dataclass
 class DataIngestionConfig:
@@ -79,5 +83,20 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
     
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, preprocessor_path = data_transformation.initiate_data_transformation(
+    train_data,
+    test_data
+)
+
     
+    model_trainer = ModelTrainer()
+    
+    r2_score_value = model_trainer.initiate_model_trainer(
+        train_arr,
+        test_arr,
+        preprocessor_path
+    )
+    
+    print("Training completed")
+    print("R2 score:", r2_score_value)
+
--- src/utils.py
+++ src/utils.py
@@ -1,33 +1,21 @@
-import os 
+print("UTILS FILE LOADED")
+
+import os
+import pickle
 import sys
+from sklearn.metrics import r2_score
 
 
-import pandas as pd
-import numpy as np
-import dill
+def save_object(file_path, obj):
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, "wb") as f:
+        pickle.dump(obj, f)
 
-from src.exception import CustomException
 
-def save_object(file_path: str, obj: object) -> None:
-    """
-    Save a Python object to a file using dill.
-
-    Args:
-        file_path (str): The path to the file where the object will be saved.
-        obj (object): The Python object to be saved.
-
-    Raises:
-        CustomException: If there is an error during the saving process.
-    """
-    try:
-        dir_path = os.path.dirname(file_path)
-        
-        
-        os.makedirs(dir_path, exist_ok=True)
-
-        with open(file_path, 'wb') as file_obj:
-            dill.dump(obj, file_obj)
-
-    except Exception as e:
-        raise CustomException(e, sys)
-    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    report = {}
+    for name, model in models.items():
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        report[name] = r2_score(y_test, y_pred)
+    return report
